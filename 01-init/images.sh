e66618c72ac16348458a8e6d6f30d1958035af3a
Dew-OF-Aurora <1962662993@qq.com>
2023-04-18 20:21:13 +0800

    创建提交

---
+++ 01-init/images.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+images=(
+kube-apiserver:v1.20.9
+kube-proxy:v1.20.9
+kube-controller-manager:v1.20.9
+kube-scheduler:v1.20.9
+coredns:1.7.0
+etcd:3.4.13-0
+pause:3.2
+)
+for imageName in ${images[@]} ; do
+docker pull registry.cn-hangzhou.aliyuncs.com/lfy_k8s_images/$imageName
+done
